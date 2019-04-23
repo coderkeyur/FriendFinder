69e12081e71c43b23071e767829f9eb0e18ccbf9
KP <cle.keyur@gmail.com>
2019-04-22 22:24:55 -0400

    updates to files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,41 @@
 
+var path = require("path");
+
 var friendsArray = require ("../data/friends");
 
 module.exports =function(app){
+
+    app.get("/api/friends", function(req,res){
+        res.json(friendsArray);
+    });
+
+    app.post("/api/friends", function(req, res){
+        var user1input = req.body;
+
+        var user1responses = user1input.scores;
+
+        var matchName="";
+        var matchImage="";
+        var totalDiff = 5000;
+
+        for (var i=0; i< friendsArray.length; i++){
+            var diff = 0;
+
+            for (var x=0; x < user1responses.length; x++){
+                diff += Math.abs(friendsArray[i].scores[x] - user1responses[x]);
+            }
+
+            if (diff < totalDiff) {
+                totalDiff = diff;
+                matchName = friendsArray[i].name;
+                matchImage = friendsArray[i].photo;
+            }
+        }
+
+        friendsArray.push(user1input);
+
+        res.json({status: "OK", matchName: matchName, matchImage: matchImage});
+
+    });
 
 };
