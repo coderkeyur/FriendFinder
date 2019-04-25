4f7f3d7df2d95e08c6cd48b4c513c7071698099f
KP <cle.keyur@gmail.com>
2019-04-25 16:41:46 -0400

    -update to server js file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 
 var express=require("express");
+var path = require("path");
 
 var app = express();
 
 var PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/app/css"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
